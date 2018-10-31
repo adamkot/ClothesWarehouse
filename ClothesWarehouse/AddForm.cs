48f0f1c5023dd2847ed308291b82fedc48bd50a5
adamkot <a.kotus@gmail.com>
2018-10-31 14:45:01 +0100

    first commit

---
+++ ClothesWarehouse/AddForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClothesWarehouse
+{
+    public partial class AddForm : Form
+    {
+        private Employer emp;
+        internal Employer Emp { get => emp; set => emp = value; }
+
+        public AddForm()
+        {
+            InitializeComponent();
+            cancelButton.CausesValidation = false; //bez validacji jeśli anulujemy
+            List<String> list = new List<string> {"test1", "test2", "test3" };
+            equipmentListBox.DataSource = list;
+            positionListBox.DataSource = list;
+        }
+
+        private void OKbutton_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren())
+            {
+                emp = new Employer();
+                emp.Name = nameTextBox.Text;
+                emp.Surname = sNameTextBox.Text;
+                emp.Position = positionListBox.Text;
+                emp.Equipment = equipmentListBox.Text;
+                emp.ExpDate = dateTimePicker.Value;
+                DialogResult = DialogResult.OK;
+            }
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void nameTextBox_Validating(object sender, CancelEventArgs e)
+        {
+            if (nameTextBox.Text.Length < 3)
+            {
+                e.Cancel = true;
+                errorProvider.SetError(nameTextBox, "wprowadź imię");
+            }
+        }
+
+        private void sNameTextBox_Validating(object sender, CancelEventArgs e)
+        {
+            if (sNameTextBox.Text.Length < 3)
+            {
+                e.Cancel = true;
+                errorProvider.SetError(sNameTextBox, "wprowadź nazwisko");
+            }
+        }
+    }
+}
---
+++ ClothesWarehouse/MainWindow.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClothesWarehouse
+{
+    public partial class MainWindow : Form
+    {
+        private List<Employer> empList;
+        private BindingSource source;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            empList = new List<Employer>();
+            var bindingList = new BindingList<Employer>(empList) { };
+            source = new BindingSource(bindingList, null);
+            mainTable.DataSource = source;
+
+            mainTable.Columns[0].HeaderText = "Imię";
+            mainTable.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            mainTable.Columns[1].HeaderText = "Nazwisko";
+            mainTable.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            mainTable.Columns[2].HeaderText = "Stanowisko";
+            mainTable.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            mainTable.Columns[3].HeaderText = "Wyposażenie";
+            mainTable.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            mainTable.Columns[4].HeaderText = "Data ważności";
+            mainTable.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        #region menu
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            addRecord();
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            editRecord();
+        }
+
+        private void delButton_Click(object sender, EventArgs e)
+        {
+            deleteRecord();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            loadListFromDB();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            saveListInDB();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            search();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            loadListFromDB();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveListInDB();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            exitProcedure();
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            addRecord();
+        }
+
+        private void editDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            editRecord();
+        }
+
+        private void delToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            deleteRecord();
+        }
+
+        private void positionListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //position list form
+        }
+
+        private void equipmentListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //equipment list form
+        }
+
+        private void viewOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //view1
+        }
+
+        private void viewInToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //view2
+        }
+
+        private void viewLastWymianyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //view3
+        }
+
+        private void homePageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //show home page
+        }
+
+        #endregion
+
+        private void addRecord()
+        {
+            AddForm addForm = new AddForm();
+            if (addForm.ShowDialog() == DialogResult.OK)
+            {
+                source.Add(addForm.Emp);
+            }
+        }
+
+        private void editRecord()
+        {
+
+            if (mainTable.CurrentCell != null)
+            {
+                EditForm editForm = new EditForm(empList[mainTable.CurrentCell.RowIndex]);
+                editForm.Emp = empList[mainTable.CurrentCell.RowIndex];
+
+                if (editForm.ShowDialog() == DialogResult.OK)
+                {
+                    source.RemoveAt(mainTable.CurrentCell.RowIndex);
+                    source.Add(editForm.Emp);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Błąd: nie zaznaczono pola)");
+            }
+            
+        }
+
+        private void deleteRecord()
+        {
+            if (mainTable.CurrentCell != null)
+            {
+                DialogResult dialogResult = MessageBox.Show("Czy chcesz usunąć pole \"" 
+                    + empList[mainTable.CurrentCell.RowIndex].Name + " " 
+                    + empList[mainTable.CurrentCell.RowIndex].Surname + "\"", "Usuń", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    source.RemoveAt(mainTable.CurrentCell.RowIndex);
+                }
+            }
+        }
+
+        private void loadListFromDB()
+        {
+            //load data from database
+        }
+
+        private void saveListInDB()
+        {
+            //send data to database
+        }
+
+        private void search()
+        {
+            //search
+        }
+
+        private void exitProcedure()
+        {
+            DialogResult dialogResult = MessageBox.Show("Czy chcesz zakończyć pracę?", "Zamknij", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Dispose();
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+    }
+}
